4ee14482c33b25a93b50de92b4db6d5dd6067b0c
udayteja96 <udaytejaganesuni@gmail.com>
2016-09-25 09:45:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send("Article one will be displayed here");
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
